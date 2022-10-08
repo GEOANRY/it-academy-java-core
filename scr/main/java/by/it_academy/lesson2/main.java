9b2ddf62e9fc5356f686d65487a50a85be2a9a35
GEOANRY <anri.bokeriya@gmail.com>
2022-10-08 19:52:04 +0400

    lesson2

---
+++ scr/main/java/by/it_academy/lesson2/main.java
@@ -0,0 +1,22 @@
+package by.it_academy.lesson2;
+
+public class main {
+
+    public static void main(String[] args) {
+        double a = 10;
+        double b = 2;
+        System.out.println("First value = " + (a));
+        System.out.println("Second value = " + (b));
+        System.out.println("Multiplication = " + (a * b));
+        System.out.println("Division = " + (a / b));
+
+        int c = 8;
+        int result = ((c + 2345) / 3) % -6;
+        System.out.println(result);
+        System.out.println(result >= -10);
+        System.out.println(result < 9);
+        System.out.println(result == 0);
+        System.out.println(result != -3);
+
+    }
+}
